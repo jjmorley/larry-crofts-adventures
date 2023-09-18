bf0820885fc0b8cb9b3d4e7898e0e572129d597c
Trent Shailer <trent.shailer@gmail.com>
2023-09-18 13:22:04 +1200

    Basic window setup

--- src/main/java/nz/ac/wgtn/swen225/lc/app/Main.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/app/Main.java
@@ -1,18 +1,32 @@
 package nz.ac.wgtn.swen225.lc.app;
-
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        GameMenuBar menuBar = new GameMenuBar();
+
+        VBox pane = new VBox();
+        pane.getChildren().add(menuBar);
+        pane.setBackground(
+                new Background(
+                        new BackgroundFill(
+                                new Color(0.000, 0.000, 0.000, 0.039),
+                                CornerRadii.EMPTY,
+                                Insets.EMPTY
+                        )
+                )
+        );
+
+        Scene scene = new Scene(pane, 720, 820);
+
+        stage.setTitle("Larry Croft's Adventures");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
