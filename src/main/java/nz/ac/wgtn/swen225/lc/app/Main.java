9337469c2be9c730948990438dea885aa2aa3a3a
sebcollis <sebcollis04@gmail.com>
2023-09-19 14:43:11 +1200

    JSON parsing works.
    JSON is parsed in and returns a 2D array.

--- src/main/java/nz/ac/wgtn/swen225/lc/app/Main.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/app/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
--- src/main/java/nz/ac/wgtn/swen225/lc/persistency/Load.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/persistency/Load.java
@@ -1,20 +1,60 @@
 package nz.ac.wgtn.swen225.lc.persistency;
 
-import org.json.*;
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.databind.*;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Loads in level from JSON file.
  */
 public class Load {
 
-    private int levelArray[][];
+    private static JsonNode json;
 
+    /**
+     * Loads JSON file from a given filepath.
+     *
+     * TODO: CHANGE FILE PATH TO A PARAMETER
+     */
     public static void loadJSON(){
-        JSONObject j = new JSONObject("levels/level-template.json");
-        System.out.println(j.toString());
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            json = objectMapper.readTree(new File("levels/levels-template.json"));
+        }
+        catch (IOException e){
+            System.out.println("Something messed up. \n" + e.toString());
+        }
     }
 
-    public int[][] loadAsArray(){
-       return null;
+    /**
+     * Return loaded JSON as an array. This is a 2D array ATM but am happy to change it.
+     *
+     * @return String[][]
+     */
+    public static String[][] loadAsArray(){
+        //JSON LOADING
+        loadJSON();
+        JsonNode level = json.get("level");
+        int gridSize = json.get("gridSize").asInt();
+
+        //MAKE ARRAY
+        String[][] levelArray = new String[gridSize][gridSize];
+        for(int i = 0; i < gridSize; i++){
+            for(int j = 0; j <  gridSize; j++){
+                levelArray[i][j] = level.get(i + ", " + j).asText();
+            }
+        }
+
+    //for testing porpoises
+//        for(int i = 0; i < gridSize; i++){
+//            for(int j = 0; j <  gridSize; j++){
+//                System.out.println(levelArray[i][j]);
+//            }
+//        }
+
+        return levelArray;
     }
 
 }
