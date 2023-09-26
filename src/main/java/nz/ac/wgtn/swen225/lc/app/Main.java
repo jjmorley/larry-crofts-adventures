120f309348f29a07e959ea50e289044587b1ef9a
Trent Shailer <trent.shailer@gmail.com>
2023-09-27 11:05:32 +1300

    Main starts a game

--- src/main/java/nz/ac/wgtn/swen225/lc/app/Main.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/app/Main.java
@@ -7,22 +7,17 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+  @Override
+  public void start(Stage stage) {
+    new Game(stage);
+  }
 
-    /**
-     * The entry point of application.
-     *
-     * @param args the input arguments
-     */
-    public static void main(String[] args) {
-        launch();
-    }
+  /**
+   * The entry point of application.
+   *
+   * @param args the input arguments
+   */
+  public static void main(String[] args) {
+    launch();
+  }
 }
