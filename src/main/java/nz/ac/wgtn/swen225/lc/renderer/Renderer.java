773def5fc59a5b93d561f664542c98aea3418f5d
morleyjose <morleyjose@ecs.vuw.ac.nz>
2023-09-28 19:35:29 +1300

    fixing bug in SpriteManager that is causing compile error

--- src/main/java/nz/ac/wgtn/swen225/lc/renderer/Renderer.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/renderer/Renderer.java
@@ -23,11 +23,14 @@ import nz.ac.wgtn.swen225.lc.domain.gameObject.Moveable.Actor;
 import nz.ac.wgtn.swen225.lc.domain.gameObject.Moveable.Direction;
 import nz.ac.wgtn.swen225.lc.domain.gameObject.Moveable.Player;
 import nz.ac.wgtn.swen225.lc.domain.gameObject.item.Item;
+import nz.ac.wgtn.swen225.lc.domain.gameObject.item.Key;
+import nz.ac.wgtn.swen225.lc.domain.gameObject.tile.Door;
 import nz.ac.wgtn.swen225.lc.domain.gameObject.tile.Tile;
 import nz.ac.wgtn.swen225.lc.domain.gameObject.tile.walkableTile.WalkableTile;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * This is the main class on the Renderer Package. It is responsible for update
@@ -55,7 +58,6 @@ public class Renderer {
 	Tile[][] tiles;
 
 
-
 	public Renderer(Domain domain, int canvasSize){
 		this.root = new Pane();
 		this.tiles = domain.getBoard().getBoard();
@@ -68,9 +70,9 @@ public class Renderer {
 		//setup player animation
 		renderPlayer(domain.getPlayer().getPosition());
 		//setup actor animation
-		if(domain.getActors() != null && !domain.getActors().isEmpty()) {
+		/**if(domain.getActors() != null && !domain.getActors().isEmpty()) {
 			renderActors(domain);
-		}
+		}**/
 	}
 
 	/**
@@ -96,20 +98,41 @@ public class Renderer {
 	 * Responsible for redrawing the static 2D tiles and items of the board.
 	 */
 	private void renderGameBoard() {
-		initialiseGameBoard();
+		//map of chars to strings which allows us to determine what colour the doors and keys should be
+		Map<Character, String> colourMap = Map.of(
+				'r', "Red",
+				'b', "Yellow",
+				'g', "Green"
+		);
+		initialiseGameBoard(); //draw an entire board of free tiles
 		GraphicsContext gc = canvas.getGraphicsContext2D();
 		// Initialize tiles and items and add them to the gridPane
 		for (int i = 0; i < tiles.length; i++) {
 			for (int j = 0; j < tiles[i].length; j++) {
 				// Load the tile image
-				Image tileImage = spriteManager.getSprite(tiles[i][j].getName());
+				Image tileImage;
+				if(tiles[i][j] instanceof Door door){//if it's a door, we need to get its associated colour
+					String doorColour = colourMap.get((char)door.getKeyID());
+					tileImage = spriteManager.getSprite("Door_"+doorColour);
+				}
+				else{
+					tileImage = spriteManager.getSprite(tiles[i][j].getName());
+				}
 				//draw the tile onto the canvas
 				gc.drawImage(tileImage, j*cellSize, i*cellSize);
+
 				//It the tile is a WalkableTile check to see it has an item
 				if(tiles[i][j] instanceof WalkableTile tile){
 					if(tile.getGameObject() != null) {
 						//load the item sprite
-						Image itemImage = spriteManager.getSprite(tile.getGameObject().getName());
+						Image itemImage;
+						if(tile.getGameObject() instanceof Key key){//if it's a key, check what colour it is
+							String keyColour  = colourMap.get((char)key.getKey());
+							itemImage = spriteManager.getSprite("Key_"+keyColour);
+						}
+						else {
+							itemImage = spriteManager.getSprite(tile.getGameObject().getName());
+						}
 						//draw the item onto the canvas
 						gc.drawImage(itemImage, j*cellSize, i*cellSize);
 					}
