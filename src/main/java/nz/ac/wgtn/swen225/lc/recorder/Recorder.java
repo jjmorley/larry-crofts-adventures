cca748ba4efde0959d9b0faf42eaaa1fc4b7f21f
nateburt <nateburt04@gmail.com>
2023-10-02 00:23:16 +1300

    Added game event class to handle multiple types of turn types

--- src/main/java/nz/ac/wgtn/swen225/lc/recorder/Recorder.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/recorder/Recorder.java
@@ -2,6 +2,7 @@ package nz.ac.wgtn.swen225.lc.recorder;
 
 
 //import nz.ac.wgtn.swen225.lc.app.Direction;
+import nz.ac.wgtn.swen225.lc.domain.gameObject.Moveable.Actor;
 import nz.ac.wgtn.swen225.lc.domain.gameObject.Moveable.Direction;
 import java.io.File;
 import java.io.IOException;
@@ -10,27 +11,93 @@ import java.util.List;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.databind.*;
+import nz.ac.wgtn.swen225.lc.domain.gameObject.Moveable.Player;
+import nz.ac.wgtn.swen225.lc.domain.gameObject.item.Item;
 
 
 class GameEvent {
     // Define your game state data here
+    private static int turn = 0;
+    private String move = "";
+    private String itemCollected = "";
+    private String actor = "";
+    private String player = "";
+
+    //game event for actor movement
+    public GameEvent(Actor actor, Direction direction){
+        turn++;
+        this.actor = actor.toString();
+        this.move = direction.toString();
+    }
+    //game event for player movement
+    public GameEvent(Player player, Direction direction) {
+        turn++;
+        this.player = player.toString();
+        this.move = direction.toString();
+
+    }
+    //game event for player item collection
+    public GameEvent(Player player, Item item){
+        turn++;
+        this.player = player.toString();
+        this.itemCollected = item.toString();
+    }
+    //game event for player attack
+    public GameEvent(Player player, Actor actor){
+        turn++;
+        this.player = player.toString();
+        this.actor = actor.toString();
+    }
+
+    public int turn(){
+        return this.turn;
+    }
+    public String move(){
+        return this.move;
+    }
+    public String item(){
+        return this.itemCollected;
+    }
+    public String player(){
+        return this.player;
+    }
+    public String actor(){
+        return this.actor;
+    }
 }
 
 
 public class Recorder {
-    private List<GameEvent> gameHistory = new ArrayList<>();
-    private List<String> moveHistory = new ArrayList<>();
+    private static List<GameEvent> gameHistory = new ArrayList<>();
+    private static List<String> moveHistory = new ArrayList<>();
 
     public void startRecording() {
         // Initialize recording
     }
 
-    public void addPlayerMove(Direction d) {
-        moveHistory.add(d.toString());
+//    public static void addPlayerMove(Direction d) {
+//        moveHistory.add(d.toString());
+//        System.out.println("Player Move '" + d.toString() + "' Recorded");
+//    }
+
+    public static void addMoveEvent(Player chap, Direction d) {
+        gameHistory.add(new GameEvent(chap,d));
         System.out.println("Player Move '" + d.toString() + "' Recorded");
     }
 
-    public void saveRecordedGameToFile(String filePath) {
+    public static void addMoveEvent(Actor actor, Direction d) {
+        gameHistory.add(new GameEvent(actor,d));
+        System.out.println("Actor Move '" + d.toString() + "' Recorded");
+    }
+    public static void addCollectEvent(Player chap, Item item) {
+        gameHistory.add(new GameEvent(chap,item));
+    }
+
+    public static void addAttackEvent(Player chap, Actor actor){
+        gameHistory.add(new GameEvent(chap,actor));
+    }
+
+    public static void saveRecordedGameToFile(String filePath) {
         try {
            ObjectMapper objectMapper = new ObjectMapper();
            objectMapper.writeValue(new File(filePath), moveHistory);
@@ -40,11 +107,11 @@ public class Recorder {
         }
     }
 
-    public void loadRecordedGameFromFile(String filePath) {
+    public static void loadRecordedGameFromFile(String filePath) {
         try {
         	//System.out.println("loaded");
             ObjectMapper objectMapper = new ObjectMapper();
-            gameHistory = objectMapper.readValue(new File(filePath), List.class);
+            moveHistory = objectMapper.readValue(new File(filePath), List.class);
         } catch (IOException e) {
             e.printStackTrace();
         }
